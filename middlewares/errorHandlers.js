44682b61b67e67bb1fb3b58d926bad51206b5133
Andrew Stark <andrew.stark@optimumpps.co.uk>
2018-03-01 15:54:19 +0000

    reset errorhandler and remove siteName variable from layout

--- middlewares/errorHandlers.js
+++ middlewares/errorHandlers.js
@@ -66,7 +66,6 @@ exports.catchErrors = (fn) => {
 
     No stacktraces are leaked to user
   */
- /*
   exports.productionErrors = (err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
@@ -74,21 +73,3 @@ exports.catchErrors = (fn) => {
       error: {}
     });
   };
-*/
-
-exports.productionErrors = (err, req, res, next) => {
-  err.stack = err.stack || '';
-  const errorDetails = {
-    message: err.message,
-    status: err.status,
-    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
-  };
-  res.status(err.status || 500);
-  res.format({
-    // Based on the `Accept` http header
-    'text/html': () => {
-      res.render('error', errorDetails);
-    }, // Form Submit, Reload the page
-    'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
-  });
-};
